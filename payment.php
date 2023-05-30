3c68685dd3317c1d5f499d605943b8b727b1c05c
viola george <violageorgekamalmansour@gmail.com>
2023-05-30 19:55:45 +0300

    pay

--- payment.php
+++ payment.php
@@ -227,6 +227,26 @@ background:#eee;
     <br><br><br><br><br><br><br><br><br>
     <main>
      <section>
+     <?php
+            include('connect.php');
+            // When form submitted, insert values into the database.
+
+
+            if (isset($_POST['submit'])) {
+                $nameoncard = $_SESSION['nameoncard'];
+                $creditcardno = $_POST['creditcardno'];
+                $expirymonth = $_POST['expirymonth'];
+                $CVV = $_POST['CVV'];
+                $query = "INSERT INTO orders(nameoncard,creditcardno, expirymonth, CVV) VALUE('$nameoncard','$creditcardno','$expirymonth','$CVV')";
+                $exe = mysqli_query($conn, $query);
+                if ($exe) {
+                    echo "Payment has recieved we'll contact you as soon as possible.";
+                    header("location:home.php");
+                } else {
+                    echo mysqli_error($conn);
+                }
+            }
+            ?>
      <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet">
 <div class="container">
 <div class="wrapper wrapper-content animated fadeInRight">
@@ -299,12 +319,12 @@ background:#eee;
 
             <div class="ibox">
                 <div class="ibox-title">
-                    Payment method
+                   <h1 style="color: black;"> Payment method </h1>
                 </div>
                 <div class="ibox-content">
 
                     <div class="panel-group payments-method" id="accordion">
-                        <div class="panel panel-default">
+                        <!-- <div class="panel panel-default">
                             <div class="panel-heading">
                                 <div class="pull-right">
                                     <i class="fa fa-cc-paypal text-success"></i>
@@ -318,7 +338,7 @@ background:#eee;
 
                                     <div class="row">
                                         <div class="col-md-10">
-                                            <h2>Summary</h2>
+                                          <h2 style="color: black  important!;">Summary</h2>
                                             
 
                                             <p class="m-t">
@@ -328,7 +348,7 @@ background:#eee;
                                                 nisi ut aliquip ex ea commodo consequat.
                                             </p>
                                             <a class="btn btn-danger">
-                                                <i class="fa fa-cc-paypal">
+                                                <i class="p-1 fa fa-cc-paypal">
                                                     Purchase via PayPal
                                                 </i>
                                             </a>
@@ -336,16 +356,17 @@ background:#eee;
                                     </div>
                                      </div>
                             </div>
-                        </div>
+                        </div> -->
                         <div class="panel panel-default">
                             <div class="panel-heading">
                                 <div class="pull-right">
-                                    <i class="fa fa-cc-amex text-success"></i>
-                                    <i class="fa fa-cc-mastercard text-warning"></i>
-                                    <i class="fa fa-cc-discover text-danger"></i>
+                                    <i class="fa fa-cc-visa payment text-success"></i>
+                                    <!-- <i class="p-1 fa fa-cc-amex text-success"></i> -->
+                                    <i class="p-1 fa fa-cc-mastercard text-warning"></i>
+                                    <i class="p-1 fa fa-cc-discover text-danger"></i>
                                 </div>
                                 <h5 class="panel-title">
-                                    <a data-toggle="collapse" data-parent="#accordion" href="#collapseTwo">Credit Card</a>
+                                    <a data-toggle="collapse" style="color: red;" data-parent="#accordion" href="#collapseTwo"><strong>By Credit Card</strong></a>
                                 </h5>
                             </div>
                             <div id="collapseTwo" class="panel-collapse collapse in">
@@ -355,54 +376,52 @@ background:#eee;
                                         <div class="col-md-4">
                                             <h2>Summary</h2>
                                            
-                                            <p class="m-t">
-                                                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-                                                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                                                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                                                nisi ut aliquip ex ea commodo consequat.
+                                            <p style="color: black;" class="m-t">
+                                            Your Informations have been Securited – as any expert should tell you – isn’t just about keeping your data secret: it’s also about ensuring it is available when you need it, and that it can be believed. In the industry jargon you ensure information’s.
 
                                             </p>
                                            
                                         </div>
                                         <div class="col-md-8">
 
-                                            <form role="form" id="payment-form">
+                                            <form role="form"action="#" method="post" class="form-group " id="payment-form" data-aos="fade-up" data-aos-delay="400" role="form">
+                                               <div class="row">
+                                                    <div class="col-xs-12">
+                                                        <div class="form-group">
+                                                            <label style="color: black;">NAME OF CARD</label>
+                                                            <input type="text" class="form-control" name="nameoncard" placeholder="NAME ON CARD">
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                               
                                                 <div class="row">
                                                     <div class="col-xs-12">
                                                         <div class="form-group">
-                                                            <label>CARD NUMBER</label>
+                                                            <label style="color: black;">EXPIRATION DATE</label>
+                                                            <input type="text" class="form-control" name="expirymonth" placeholder="MM / YY" required="">
+                                                        </div>
+                                                    </div>
+                                                    <div class="col-xs-5 col-md-5 pull-right">
+                                                        <div class="form-group">
+                                                            <label style="color: black;">CV CODE</label>
+                                                            <input type="text" class="form-control" name="CVV" placeholder="CVC" required="">
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <div class="row">
+                                                    <div class="col-xs-12">
+                                                        <div class="form-group">
+                                                            <label style="color: black;">CARD NUMBER</label>
                                                             <div class="input-group">
-                                                                <input type="text" class="form-control" name="Number" placeholder="Valid Card Number" required="">
-                                                                <span class="input-group-addon"><i class="fa fa-credit-card"></i></span>
+                                                                <input type="text" class="form-control" name="creditcardno" placeholder="Valid Card Number" required="">
+                                                                <span class="input-group-addon"><i style="font-size: 26px;" class="pl-3  fa fa-credit-card"></i></span>
                                                             </div>
                                                         </div>
                                                     </div>
                                                 </div>
                                                 <div class="row">
                                                     <div class="col-xs-12">
-                                                        <div class="form-group">
-                                                            <label>EXPIRATION DATE</label>
-                                                            <input type="text" class="form-control" name="Expiry" placeholder="MM / YY" required="">
-                                                        </div>
-                                                    </div>
-                                                    <div class="col-xs-5 col-md-5 pull-right">
-                                                        <div class="form-group">
-                                                            <label>CV CODE</label>
-                                                            <input type="text" class="form-control" name="CVC" placeholder="CVC" required="">
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                                <div class="row">
-                                                    <div class="col-xs-12">
-                                                        <div class="form-group">
-                                                            <label>NAME OF CARD</label>
-                                                            <input type="text" class="form-control" name="nameCard" placeholder="NAME AND SURNAME">
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                                <div class="row">
-                                                    <div class="col-xs-12">
-                                                        <button class="btn btn-danger" type="submit">Make a payment!</button>
+                                                        <button class="mt-3 btn btn-danger" type="submit">Make a payment!</button>
                                                     </div>
                                                 </div>
                                             </form>
